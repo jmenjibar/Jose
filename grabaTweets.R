59da4df0f638d9c5fac80c3c1c2f97cdef7de549
Jose Menjibar <jmenjibarentrena@gmail.com>
2016-04-19 16:43:57 +0200

    Palabras usadas en mi busqueda para el tfg

--- grabaTweets.R
+++ grabaTweets.R
@@ -18,15 +18,38 @@ setup_twitter_oauth (api_key[["Key"]],api_key[["Secret"]],api_key[["Token"]],api
   
 #Para buscar cosas en Twitter  
 
-#Creamos una variable que almacena los terminos de busqueda
-busqueda <- "Otegui"
-#fecha de hoy
+#Fecha de hoy
 fecha <- as.character(Sys.Date())
+
+#Creamos una variable que almacena los terminos de busqueda
+busqueda1 <- "estado islamico"
+busqueda2 <- "en Iraq"
+busqueda3 <- "califato"
+busqueda4 <- "allah"
+busqueda5 <- "yihad"
+
+
 #Buscar tweets con fecha de hoy
 # los almacenamos en una variable, que es una lista de objetos
-searchTwitteR(busqueda, since=fecha, n=5000) -> twits
+searchTwitteR(busqueda1, since=fecha, n=5000, lang="es") -> twits1
+searchTwitteR(busqueda2, since=fecha, n=5000, lang="es") -> twits2
+searchTwitteR(busqueda3, since=fecha, n=5000, lang="es") -> twits3
+searchTwitteR(busqueda4, since=fecha, n=5000, lang="es") -> twits4
+searchTwitteR(busqueda5, since=fecha, n=5000, lang="es") -> twits5
 
 
 #Graba twits en fichero
-nombreFichero <- sprintf("Datos/%s.rds", fecha)
-saveRDS(twits, nombreFichero)
+nombreFichero1 <- sprintf("Datos/%s_%s.rds", fecha, "estado_islamico")
+saveRDS(twListToDF(twits1), nombreFichero1)
+nombreFichero2 <- sprintf("Datos/%s_%s.rds", fecha, "en_Iraq")
+saveRDS(twListToDF(twits2), nombreFichero2)
+nombreFichero3 <- sprintf("Datos/%s_%s.rds", fecha, "califato")
+saveRDS(twListToDF(twits3), nombreFichero3)
+nombreFichero4 <- sprintf("Datos/%s_%s.rds", fecha, "allah")
+saveRDS(twListToDF(twits4), nombreFichero4)
+nombreFichero5 <- sprintf("Datos/%s_%s.rds", fecha, "yihad")
+saveRDS(twListToDF(twits5), nombreFichero5)
+
+#Para recuperar la base de datos: ttt <- readRDS(sprintf("Datos/%s.rds", fecha))
+#Para que devuelva un twit: ttt[[1]]
+
