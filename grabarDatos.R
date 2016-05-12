5ecc27be0e9f6d1fcf3a85e6820af5749a914bcc
jmenjibar <jmenjibarentrena@gmail.com>
2016-05-12 16:40:57 +0200

    Aplicación para pasar datos a documentos de texto añadida al repositorio

---
+++ grabarDatos.R
@@ -0,0 +1,28 @@
+
+# Esta funcion va a leeer un fichero .rds y extraera el texto. Un solo fichero.
+library(twitteR)
+library (tm)
+library(wordcloud)
+library(cluster)
+
+grabaTextoTweet <- function(fichero, dir="Datos", dirTxt="txt") {
+  nf<-sprintf("%s/%s", dir, fichero)
+  listTweets <- strip_retweets(readRDS(nf))
+  sapply(listTweets, function(x) TweetTexto(x, dirTxt))
+  
+  
+}
+#Esta funcion, graba el texto de un tweet en un fichero.
+TweetTexto <- function(t, directorio="txt"){
+  nf <- sprintf("%s/%s.txt", directorio, t$id)
+  writeBin(t$text, nf, useBytes = TRUE)
+}
+#Esta funcion te saca todos los tweets a texto
+GrabaTodosDatosTxt <- function(dirDatos="Datos", dirTxt="txt"){
+  ficheros <- list.files(dirDatos)
+  sapply(ficheros, function(x) grabaTextoTweet(x, dirDatos, dirTxt))
+}
+
+
+
+
